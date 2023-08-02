2a92907e428fba4e6db5000ef5050073fca0b8a8
gasser <gasserkhaled23@gmail.com>
2023-08-02 19:11:51 +0300

    compelete phase 2 doubly linked list

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -1,22 +1,42 @@
 class DoublyLinkedListNode {
-  constructor(val) {
+  constructor(val, previous, next) {
     this.value = val;
-    this.prev = null;
-    this.next = null;
+    this.prev = previous || null;
+    this.next = next || null;
   }
 }
 
 class DoublyLinkedList {
   constructor() {
-    // Your code here
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
   addToHead(val) {
-    // Your code here
+    const newNode = new DoublyLinkedListNode(val);
+    if (!this.head) this.head = this.tail = newNode;
+    else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
   }
 
   addToTail(val) {
-    // Your code here
+    const newNode = new DoublyLinkedListNode(val);
+    if (!this.head) this.tail = this.head = newNode;
+    else {
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = newNode;
+      newNode.prev = current;
+      this.tail = newNode;
+    }
+    this.length++;
   }
 
   // You can use this function to help debug
